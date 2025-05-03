f5f72ca34bbbb3e0cc10f98a9bf78264723ca720
mo-raies <raiesmohammadhindoli0786@gmail.com>
2025-05-03 13:41:19 +0530

    ADDED URLS

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import io from "socket.io-client";
 import { setSocket } from "./redux/socketSlice";
 import { setOnlineUsers } from "./redux/userSlice";
-import { BASE_URL } from "./utils/constant";
+import { BASE_URL, SOCKET_BASE_URL } from "./utils/constant";
 import Signup from "./Components/Signup";
 import HomePage from "./Components/Homepage";
 import Login from "./Components/Login";
@@ -33,7 +33,7 @@ function App() {
 
   useEffect(() => {
     if (authUser) {
-      const socketio = io(`http://localhost:8080`, {
+      const socketio = io(SOCKET_BASE_URL, {
         query: {
           userId: authUser._id,
         },
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,4 @@
 
-export const BASE_URL = "http://localhost:8080/api/v1";
+// export const BASE_URL = "http://localhost:8080/api/v1";
+export const BASE_URL = "https://chat-app-backend-n191.onrender.com/api/v1";
+export const SOCKET_BASE_URL = "https://chat-app-backend-n191.onrender.com";
