6dc67367e9c699fd05a1ee91b2b83d1445526dd5
mo-raies <raiesmohammadhindoli0786@gmail.com>
2025-05-03 13:18:47 +0530

    test

--- src/Components/OtherUsers.jsx
+++ src/Components/OtherUsers.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import OtherUser from "./OtherUser";
+import OtherUser from "./OtherUser.jsx";
 import useGetOtherUsers from "../hooks/useGetOtherUsers";
 import { useSelector } from "react-redux";
 
